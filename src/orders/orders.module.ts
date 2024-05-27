939301bfa8b6d7447d347d970ac361fab8d8673a
Thong Le <anhthong21524@gmail.com>
2024-05-27 23:14:51 +0700

    using product client

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -5,12 +5,13 @@ import { OrderRepository } from './repositories/order.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
 import { Module } from '@nestjs/common';
+import { ProductClient } from './repositories/clients/product.client';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrderController],
-  providers: [OrderService, OrderRepository],
+  providers: [OrderService, OrderRepository, ProductClient],
 })
 export class OrdersModule {}
